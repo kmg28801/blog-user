1344d809dbc8e45417b9f5a98e76c1c1bf89f8af
kmg28801 <kmg08801@gmail.com>
2022-03-04 20:18:05 +0900

    user service initialize

---
+++ src/main/java/tj/kmg/blog/user/BlogUserApplication.java
@@ -0,0 +1,15 @@
+package tj.kmg.blog.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BlogUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tj/kmg/blog/user/BlogUserApplicationTests.java
@@ -0,0 +1,13 @@
+package tj.kmg.blog.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
